36f0cc25001a0381ac71c65977084e920b418c94
Hazem Samak <hazem.samak@gmail.com>
2022-11-06 01:08:51 +0400

    Remove JWT config

--- app.py
+++ app.py
@@ -20,13 +20,5 @@ def create_app(db_url=None):
     app.config["PROPAGATE_EXCEPTIONS"] = True
     api = Api(app)
 
-    """
-    JWT related configuration. The following functions includes:
-    1) add claims to each jwt
-    2) customize the token expired error message
-    """
-    app.config["JWT_SECRET_KEY"] = "jose"
-   
-
     api.register_blueprint(RateBlueprint)
     return app
