ba47837d02b45f4839174e8770f8827d4c1c58df
Hazem Samak <hazem.samak@gmail.com>
2022-11-10 17:04:54 +0400

    Add api-key to secure  API

---
+++ components/auth/decorators.py
@@ -0,0 +1,40 @@
+__author__ = 'hsamak'
+
+from functools import wraps
+from flask import request
+from flask_smorest import  abort
+
+api_key = "49c5bc127e5d85080d399a5a0010161e"
+
+def match_api_keys(key):
+    """
+    Match API keys and discard ip
+    @param key: API key from request
+    @param ip: remote host IP to match the key.
+    @return: boolean
+    """
+    if key is None:
+        return False
+
+    if api_key == key:
+        return True
+    return False
+
+
+def require_app_key(f):
+    """
+    @param f: flask function
+    @return: decorator, return the wrapped function or abort json object.
+    """
+
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        # if match_api_keys(request.args.get('api-key')):
+        if request.headers.get('x-api-key') and match_api_keys(request.headers.get('x-api-key')):
+            return f(*args, **kwargs)
+            # return f(*args, **kwargs)
+        else:
+            abort(401, "Please send a valid 'api-key'")
+
+    return decorated
+
---
+++ components/auth/helpers.py
@@ -0,0 +1,15 @@
+__author__ = 'hsamak'
+
+import hashlib
+import os
+
+
+
+def generate_hash_key():
+    """
+    @return: A hashkey for use to authenticate agains the API.
+    """
+    return hashlib.md5(os.urandom(32)).hexdigest()
+
+
+print(generate_hash_key())
